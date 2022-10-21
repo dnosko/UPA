92b036728e11abed27daae827fdd4a905b811b8b
Martin Osvald <Martin.Osvald@ibm.com>
2022-10-21 11:14:56 +0200

    fix server

--- server/main.py
+++ server/main.py
@@ -4,10 +4,7 @@ from flask_pymongo import PyMongo
 
 application = Flask(__name__)
 
-application.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
 
-mongo = PyMongo(application)
-db = mongo.db
 
 @application.route('/')
 def index():
@@ -49,5 +46,5 @@ def createTodo():
 
 if __name__ == "__main__":
     ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
-    ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5000)
+    ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5001)
     application.run(host='0.0.0.0', port=ENVIRONMENT_PORT, debug=ENVIRONMENT_DEBUG)
