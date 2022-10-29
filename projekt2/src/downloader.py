1908884eaa1c9fe40f96df525a91b1e8fb12010c
Mylanos <marekziska99@gmail.com>
2022-10-30 01:49:22 +0200

    Structure, dataset download, conversion to pandas dataframes, columns cleanup

---
+++ projekt2/src/downloader.py
@@ -0,0 +1,47 @@
+import os
+import numpy as np
+from pathlib import Path
+from kaggle.api.kaggle_api_extended import KaggleApi
+from os import listdir, path
+from src.constants import NUM_OF_EXPECTED_CVS
+
+class DataDownloader:
+
+    def __init__(self, DATASET_PATH = "parulpandey/2020-it-salary-survey-for-eu-region", folder='data/download_data/'):
+
+        self.api = KaggleApi()
+        self.api.authenticate()
+        self.dataset_path = DATASET_PATH
+        main_folder = Path(__file__).parent.parent.resolve()
+        self.data_folder = main_folder / folder
+        self.data_files = []
+
+        if not os.path.isdir(self.data_folder):
+            try:
+                print("Creating directory...")
+                os.mkdir(folder)
+            except OSError:
+                print("Creation of the directory %s failed" % os.path)
+
+        self.update_downloaded_files()
+    
+    def download_data(self):
+        print(self.data_folder)
+        self.api.dataset_download_files(dataset=self.dataset_path, path=self.data_folder, unzip=True)
+    
+    def update_downloaded_files(self):
+        if os.listdir(self.data_folder) == [] or len(os.listdir(self.data_folder)) < NUM_OF_EXPECTED_CVS:
+            self.download_data()
+        # iterate over files in data directory
+        for filename in listdir(self.data_folder):
+            f = path.join(self.data_folder, filename)
+            # checking if it is a file
+            if path.isfile(f):
+                # store the name of the downloaded file
+                if(filename not in self.data_files):
+                    self.data_files.append(str(self.data_folder ) + "/"+  filename)
+
+
+if __name__ == "__main__":
+    downloader = DataDownloader()
+    downloader.download_data()
---
+++ projekt2/src/parser.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import re
+
+class DataParser:
+
+    def __init__(self, folder, files=[]):
+        self.data_folder = folder
+        self.files = files
+        self.dataframes = {}
+
+    def parse_data(self):
+        cleaned_df = pd.DataFrame()
+
+        for file in self.files:
+            with open(file, mode='r') as csv_file:
+                df = pd.read_csv(csv_file)
+                # cleaning up the names of the columns
+                if(re.match("(\w|[0-9]|/| )*2019.csv", file)):
+                    df = df.rename(columns={"Zeitstempel": "Timestamp", "Position (without seniority)": "Position"})
+                    self.dataframes["2019"] = df
+                elif(re.match("(\w|[0-9]|/| )*2018.csv", file)):
+                    df = df.rename(columns={"Zeitstempel": "Timestamp", "Position (without seniority)": "Position", "Your level": "Seniority level"})
+                    self.dataframes["2018"] = df
+                else:
+                    # dropping columns with almost no values or with the same values in most of the rows
+                    df = df.drop(['Have you received additional monetary support from your employer due to Work From Home? If yes, how much in 2020 in EUR',
+                                'Have you been forced to have a shorter working week (Kurzarbeit)? If yes, how many hours per week'], axis=1)
+                    df = df.rename(columns={"Have you lost your job due to the coronavirus outbreak?": "Lost job due to Covid", "Total years of experience": "Years of experience"})
+                    self.dataframes["2020"] = df
+                if cleaned_df.empty:
+                    cleaned_df = df.filter(['Timestamp'], axis=1)
