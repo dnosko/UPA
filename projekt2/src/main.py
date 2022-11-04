cfc8a8001add53549bc8fee7698358d80a91474a
Daša Nosková <dasa.noskova@avast.com>
2022-11-04 23:48:05 +0100

    ranges, types of values

--- projekt2/src/main.py
+++ projekt2/src/main.py
@@ -1,11 +1,14 @@
 from src.downloader import DataDownloader
 from src.parser import DataParser
+from src.dataanalysis import DataAnalysis
 
 def main():
     downloader = DataDownloader()
     parser = DataParser(downloader.data_folder, downloader.data_files)
     parser.parse_data()
-    print(parser.dataframes["2019"])
+    analyzer = DataAnalysis(parser.dataframes['2019'])
+    analyzer.analyze_attribute()
+    #print(parser.dataframes["2019"])
 
 if __name__ == "__main__":
     main()
