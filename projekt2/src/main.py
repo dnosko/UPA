400759e02c79545e22374b38f6f21f0fd43f7051
Daša Nosková <dasa.noskova@gmail.com>
2022-11-05 12:09:08 +0100

    penguins

--- projekt2/src/main.py
+++ projekt2/src/main.py
@@ -5,9 +5,9 @@ from src.dataanalysis import DataAnalysis
 def main():
     downloader = DataDownloader()
     parser = DataParser(downloader.data_folder, downloader.data_files)
-    parser.parse_data()
-    df = parser.concat_all_df()
-    analyzer = DataAnalysis(df)
+    #parser.parse_data()
+    print(parser.df)
+    #analyzer = DataAnalysis(df)
 
 if __name__ == "__main__":
     main()
