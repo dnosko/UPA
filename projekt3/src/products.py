109cc1b492e4bac8516d1a71993f484fa6980c63
Daša Nosková <dasa.noskova@avast.com>
2022-12-01 01:55:02 +0100

    delete debug code

--- projekt3/src/products.py
+++ projekt3/src/products.py
@@ -19,15 +19,11 @@ headers = {
             'cookie': '__uzma=1f22a0b0-baa6-4fa9-a71d-451e2c8fcd6a; __uzmb=1602620116; __uzme=4861; VZTX=2733254163; TPL=1; CCC=18863907; CriticalCSS=6858194; .AspNetCore.Culture=c%3Dcs-CZ%7Cuic%3Dcs-CZ; _vwo_uuid_v2=D1424839DBF8E18D19F6126EFE4B45DC9|c4261905652b4bb599d831078fc529a4; __ssds=2; __ssuzjsr2=a9be0cd8e; __uzmaj2=872c22d0-babe-4a28-a326-b816a5c808b0; __uzmbj2=1602677383; i18next=cs-CZ; _gid=GA1.2.483098774.1602677384; ai_user=02ajXPMWLYcA82LQmcgmKt|2020-10-14T12:09:44.442Z; _gcl_au=1.1.613976267.1602677389; db_ui=c811ca53-36f3-5c15-45e5-355854a4ebe2; _hjTLDTest=1; _hjid=a08844ad-48f2-4648-b256-ec75750846ce; _hjIncludedInSessionSample=1; _hjAbsoluteSessionInProgress=1; db_uicd=454c9966-c325-ef24-03c8-dd90e750ee8a; PVCFLP=5; __uzmcj2=664922256883; __uzmdj2=1602677571; lb_id=b0ae2fcde80aefdf082845c369118483; SL_C_23361dd035530_KEY=178242482b9ce6820d21aac111dd0e23835adf09; __uzmc=956408239211; __uzmd=1602678241; _ga=GA1.1.1771958577.1602677384; _ga_FGLGFS7LP0=GS1.1.1602677388.1.1.1602678264.60; SL_C_23361dd035530_SID=5C40vMURNg; SL_C_23361dd035530_VID=-ibqW9xeUh6; ai_session=XhPbnATpFs7KMd15JD5gZl|1602677386866|1602678448923'
         }
 
-def get_urls(input_file:str, output_file="data.tsv"):
+def get_urls(input_file='urls.txt', output_file="data.tsv"):
     urls = open(input_file, "r")
-    f_output = open(output_file, "a")
+    f_output = open(output_file, "w")
     csv_output = csv.writer(f_output, delimiter='\t')
-    i = 0
     for url in urls:
-        i = i +1
-        if i < 83:
-            continue
         url = url.replace('\n', '')
         request = Request(url, headers=headers)
         page = urlopen(request)
@@ -42,6 +38,9 @@ def get_urls(input_file:str, output_file="data.tsv"):
     f_output.close()
 
 if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print('Script needs input file with urls.')
+        exit(0)
     input_file = sys.argv[1]
     output_file = "data.tsv"
     if len(sys.argv) > 2:
