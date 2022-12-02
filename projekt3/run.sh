2936c869486f2b2faf0692e1d204e7da1cf40ef7
Daša Nosková <dasa.noskova@avast.com>
2022-12-02 14:10:20 +0100

    fix urls

--- projekt3/run.sh
+++ projekt3/run.sh
@@ -7,12 +7,12 @@ URLS="src.urls"
 PRODUCTS="src.products"
 tmpfile=$(mktemp)
 output="$PWD/data_20.tsv"
-echo `head -20 $PWD/urls.txt > $tmpfile`
 
 . build.sh
 echo $VIRTUAL_ENV
 $PYTHON -m $URLS
 
+echo `head -20 $PWD/urls.txt > $tmpfile`
 
 $PYTHON -m $PRODUCTS $tmpfile $output
 rm "$tmpfile"
